eb5dc7b3c3058ab9b2e77670f02a5a20c77d5f62
DontLookAthis <DontLookAthis@users.noreply.github.com>
2018-10-24 12:22:08 +1300

    -Fixing

--- DD_YY_MM/Source/ManagementGame/ManagementGame.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
-
-#include "ManagementGame.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ManagementGame, "ManagementGame" );
-
-DEFINE_LOG_CATEGORY(LogManagementGame)
- 
